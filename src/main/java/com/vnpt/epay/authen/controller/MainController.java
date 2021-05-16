c5bdd56dd9f2bd6cdbcaaa8f6e0f1e75f1d67350
unknown <cuongdq1810@gmail.com>
2021-05-16 10:28:14 +0700

    update

--- src/main/java/com/vnpt/epay/authen/controller/MainController.java
+++ src/main/java/com/vnpt/epay/authen/controller/MainController.java
@@ -1,70 +1,103 @@
 package com.vnpt.epay.authen.controller;
 
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 import com.vnpt.epay.authen.entity.User;
 import com.vnpt.epay.authen.service.IUserService;
 import com.vnpt.epay.authen.service.UserDetailsServiceImpl;
 import com.vnpt.epay.authen.service.UserSevice;
 
+import dev.samstevens.totp.qr.QrData;
+import dev.samstevens.totp.qr.QrDataFactory;
+import dev.samstevens.totp.qr.QrGenerator;
+import dev.samstevens.totp.secret.SecretGenerator;
+
 @Controller
 public class MainController {
 	@Autowired
 	UserDetailsServiceImpl userDetailsServiceImpl;
-	
+
 	@Autowired
 	UserSevice userSevice;
-	
+
 	@Autowired
-	IUserService iUserService;
+	SecretGenerator secretGenerator;
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
+	@Autowired
+	private QrDataFactory qrDataFactory;
 
-    @GetMapping("/signin") 
-    public String admin() {
-        return "admin";
-    }
+	@Autowired
+	private QrGenerator qrGenerator;
 
-    @GetMapping("/403")
-    public String accessDenied() {
-        return "403";
-    }
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
 
-    @GetMapping("/login") 
-    public String getLogin() {
-        return "login";
-    }
-    
-    @GetMapping("/registrationConfirm")
-    public String confirmRegistration(final HttpServletRequest request,final ModelMap model,@RequestParam("token") String token) throws UnsupportedEncodingException {
-    	Locale locale = request.getLocale();
-        String result = iUserService.validateVerificationToken(token);
-        if(result.equals("valid")) {
-            User user = userSevice.findBySecret(token);
-            if (user.isAdditionalSecurity()) {
-                model.addAttribute("qr", userSevice.generateQRUrl(user));
-                return "redirect:/qrcode.html?lang=" + locale.getLanguage();
-            }
-            
-            model.addAttribute(
-              "message", "save");
-            return "redirect:/login?lang=" + locale.getLanguage();
-        }
-		return result;
-        
-        
-       
-    }
+	@GetMapping("/signin")
+	public String admin() {
+		return "admin";
+	}
+
+	@GetMapping("/signup")
+	public String signup() {
+		
+		return "/signup";
+	}
+	
+	 @RequestMapping(value = "/qr", method = RequestMethod.POST)
+	    public String submitCreateUser(Model model, @RequestParam HashMap<String, String> reqParams, RedirectAttributes redirectAttributes) throws UnsupportedEncodingException {
+	    		User user = new User();
+		    	String email = reqParams.get("email");
+		        String password = reqParams.get("password");
+		        
+		        user.setEmail(email);
+		        user.setPassword(password);
+		        String secret = secretGenerator.generate();
+		        user.setSecret(secret);
+
+//		        QrData data = qrDataFactory.newBuilder()
+//		            .label(email)
+//		            .secret(secret)
+//		            .issuer("AuthenWeb")
+//		            .build();
+//		        String qrCodeImage = getDataUriForImage(
+//		                qrGenerator.generate(data), 
+//		                qrGenerator.getImageMimeType()
+//		              );
+		        
+		        String   qrUrl = userSevice.generateQRUrl(user);
+		        model.addAttribute("qrurl", qrUrl);
+		        
+		        return "/qr";
+	     
+	    }
+	@GetMapping("/403")
+	public String accessDenied() {
+		return "403";
+	}
+
+	@GetMapping("/login")
+	public String getLogin() {
+		return "login";
+	}
+
 }
